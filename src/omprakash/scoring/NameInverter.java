21e8e3d1af6ea0e2501c06608a18742a86ddea44
Omprakash <omprakash.mopuri@improve10x.com>
2023-07-13 11:56:04 +0530

    Implemented name inverter problem by using TDD

---
+++ src/omprakash/scoring/NameInverter.java
@@ -0,0 +1,38 @@
+package omprakash.scoring;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class NameInverter {
+    public String invertName(String name) {
+        if (name.equals("")) {
+            return "";
+        } else if (!name.trim().contains(" ")) {
+            return name.trim();
+        } else {
+            return getFormatedName(name);
+        }
+    }
+
+    private static String getFormatedName(String name) {
+        ArrayList<String> names = new ArrayList<>(Arrays.asList(name.split("\\s+")));
+        removeHonorifics(names);
+        String postNominal = getPostNominal(names.subList(2, names.size()));
+        return (names.get(1) + " " + names.get(0) + " " + postNominal).trim();
+    }
+
+    private static String getPostNominal(List<String> names) {
+        String postNominal = "";
+        for (int i = 0; i < names.size(); i++) {
+            postNominal = names.get(i) + " ";
+        }
+        return postNominal;
+    }
+
+    private static void removeHonorifics(ArrayList<String> names) {
+        if (names.get(0).matches("Mrs.|Mr.|Miss") && names.size() > 2) {
+            names.remove(0);
+        }
+    }
+}
---
+++ src/omprakash/scoring/NameInverterTest.java
@@ -0,0 +1,56 @@
+package omprakash.scoring;
+
+import org.junit.Assert;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class NameInverterTest {
+
+    private NameInverter nameInverter = new NameInverter();
+
+    @Test
+    public void nothing() {
+    }
+
+    @Test
+    public void givenEmpty_returnsEmpty() {
+        String invertedName = nameInverter.invertName("");
+        assertEquals("", invertedName);
+    }
+
+    @Test
+    public void givenSingleWord_returnsSingleWord() {
+        String invertedName = nameInverter.invertName("first");
+        assertEquals("first", invertedName);
+    }
+
+    @Test
+    public void givenSingleWordWithTrialingSpaces_returnsSingleWord() {
+        String invertedName = nameInverter.invertName("first     ");
+        assertEquals("first", invertedName);
+    }
+
+    @Test
+    public void givenSingleWordWithSpaces_returnsSingleWord() {
+        String invertedName = nameInverter.invertName("    first     ");
+        assertEquals("first", invertedName);
+    }
+
+    @Test
+    public void givenFirstLast_returnsLastFirst() {
+        String invertedName = nameInverter.invertName("first last");
+        assertEquals("last first", invertedName);
+    }
+
+    @Test
+    public void givenHonorifics_returnsInvertedName() {
+        String invertedName = nameInverter.invertName("Mrs. first last");
+        assertEquals("last first", invertedName);
+    }
+
+    @Test
+    public void givenPostNominals_returnsInvertedName() {
+        String invertedName = nameInverter.invertName("first last Phd.");
+        assertEquals("last first Phd.", invertedName);
+    }
+}
