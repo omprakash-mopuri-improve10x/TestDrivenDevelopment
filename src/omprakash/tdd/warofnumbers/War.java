7ac675be846b2c1d80b8df776b03cbb28ef6ed35
Omprakash <omprakash.mopuri@improve10x.com>
2023-07-19 10:57:35 +0530

    Implemented givenEmpty_returnsMinusOne test case

--- src/omprakash/tdd/warofnumbers/War.java
+++ src/omprakash/tdd/warofnumbers/War.java
@@ -2,7 +2,7 @@ package omprakash.tdd.warofnumbers;
 
 public class War {
     public int warOfNumbers(int[] nums) {
-        if (nums == null) {
+        if (nums == null || nums.length == 0) {
             return -1;
         }
         return 0;
--- src/omprakash/tdd/warofnumbers/WarTest.java
+++ src/omprakash/tdd/warofnumbers/WarTest.java
@@ -6,14 +6,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class WarTest {
 
+    private War war = new War();
+
     @Test
     public void nothing() {
     }
 
     @Test
     public void givenNull_returnsMinusOne() {
-        War war = new War();
         int result = war.warOfNumbers(null);
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenEmpty_returnsMinusOne() {
+        int result = war.warOfNumbers(new int[] {});
         assertEquals(-1, result);
     }
 }
