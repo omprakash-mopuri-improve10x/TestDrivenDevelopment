24448ef4702117744d1f4925245649b2c7f942ae
Omprakash <omprakash.mopuri@improve10x.com>
2023-07-19 12:54:32 +0530

    Implemented getMaxGap() method in MaximumGap.java

--- src/omprakash/tdd/maximumgap/MaximumGap.java
+++ src/omprakash/tdd/maximumgap/MaximumGap.java
@@ -1,11 +1,19 @@
 package omprakash.tdd.maximumgap;
 
 public class MaximumGap {
+
     public int getMaxGap(int[] nums) {
-        int result = 0;
-        if (nums == null || nums.length == 0) {
-            result = -1;
+        int maxGap = 0;
+        if (nums == null || nums.length <= 1) {
+            maxGap = -1;
+        } else {
+            for (int i = 0; i < nums.length - 1; i++) {
+                int gap = nums[i] > nums[i + 1] ? nums[i] - nums[i + 1]: nums[i + 1] - nums[i];
+                if (gap > maxGap) {
+                    maxGap = gap;
+                }
+            }
         }
-        return result;
+        return maxGap;
     }
 }
--- src/omprakash/tdd/maximumgap/MaximumGapTest.java
+++ src/omprakash/tdd/maximumgap/MaximumGapTest.java
@@ -1,6 +1,5 @@
 package omprakash.tdd.maximumgap;
 
-import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,5 +22,47 @@ public class MaximumGapTest {
         MaximumGap maximumGap = new MaximumGap();
         int result = maximumGap.getMaxGap(new int[] {});
         assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenSingleNumber_returnsMinusNumber() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {1});
+        assertEquals(-1, result);
+    }
+
+    @Test
+    public void givenOneTwo_returnsOne() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {1, 2});
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void givenOneTwoFourTen_returnsSix() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {1, 2, 4, 10});
+        assertEquals(6, result);
+    }
+
+    @Test
+    public void givenMinusOneMinusTwo_returnsOne() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {-1, -2});
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void givenMinusOneTwo_returnsThree() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {-1, 2});
+        assertEquals(3, result);
+    }
+
+    @Test
+    public void givenMinusOneMinusTwoTwoFour_returnsFour() {
+        MaximumGap maximumGap = new MaximumGap();
+        int result = maximumGap.getMaxGap(new int[] {-1, -2, 2, 4});
+        assertEquals(4, result);
     }
 }
