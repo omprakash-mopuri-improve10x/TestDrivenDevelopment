b40614b8c31f760aaeb46860193de7c33bd18667
Omprakash <omprakash.mopuri@improve10x.com>
2023-07-19 11:18:14 +0530

    Implemented negative numbers test cases

--- src/omprakash/tdd/warofnumbers/WarTest.java
+++ src/omprakash/tdd/warofnumbers/WarTest.java
@@ -29,4 +29,34 @@ public class WarTest {
         int result = war.warOfNumbers(new int[] {1, 2});
         assertEquals(1, result);
     }
+
+    @Test
+    public void givenOne_returnsOne() {
+        int result = war.warOfNumbers(new int[] {1});
+        assertEquals(1, result);
+    }
+
+    @Test
+    public void givenOneThreeFour_returnsZero() {
+        int result = war.warOfNumbers(new int[] {1, 3, 4});
+        assertEquals(0, result);
+    }
+
+    @Test
+    public void givenLongInput_returnsProperResult() {
+        int result = war.warOfNumbers(new int[] {5, 9, 45, 6, 2, 7, 34, 8, 6, 90, 5, 243});
+        assertEquals(168, result);
+    }
+
+    @Test
+    public void givenMinusOneThree_returnsTwo() {
+        int result = war.warOfNumbers(new int[] {-1, 3});
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void givenWithNegativeNumbers_returnsProperResult() {
+        int result = war.warOfNumbers(new int[] {-1, 3, -2 , -4, 4});
+        assertEquals(4, result);
+    }
 }
