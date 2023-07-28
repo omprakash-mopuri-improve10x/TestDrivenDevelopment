446ccf549020886f3f59069058bb35a8bfd27ead
Omprakash <omprakash.mopuri@improve10x.com>
2023-07-28 12:19:49 +0530

    Implemented findPrimorial method by using TDD

--- src/omprakash/tdd/Primorial.java
+++ src/omprakash/tdd/Primorial.java
@@ -1,7 +1,30 @@
 package omprakash.tdd;
 
 public class Primorial {
+
     public int findPrimorial(int n) {
-        return -1;
+        int primorial = 1;
+        if (n < 1) {
+            primorial = -1;
+        } else {
+            int primesCount = 0;
+            for (int i = 1; i < Integer.MAX_VALUE; i++) {
+                if (primesCount != n) {
+                    int factors = 0;
+                    for (int j = 1; j <= i; j++) {
+                        if (i % j == 0) {
+                            factors++;
+                        }
+                    }
+                    if (factors == 2) {
+                        primesCount++;
+                        primorial *= i;
+                    }
+                } else {
+                    break;
+                }
+            }
+        }
+        return primorial;
     }
 }
--- src/omprakash/tdd/PrimorialTest.java
+++ src/omprakash/tdd/PrimorialTest.java
@@ -27,7 +27,28 @@ public class PrimorialTest {
     @Test
     public void givenZero_returnMinusOne() {
         Primorial primorial = new Primorial();
-        int negativePrimorial = primorial.findPrimorial(0);
-        assertEquals(-1, negativePrimorial);
+        int zeroPrimorial = primorial.findPrimorial(0);
+        assertEquals(-1, zeroPrimorial);
+    }
+
+    @Test
+    public void givenOne_returnTwo() {
+        Primorial primorial = new Primorial();
+        int onePrimorial = primorial.findPrimorial(1);
+        assertEquals(2, onePrimorial);
+    }
+
+    @Test
+    public void givenTwo_returnSix() {
+        Primorial primorial = new Primorial();
+        int onePrimorial = primorial.findPrimorial(2);
+        assertEquals(6, onePrimorial);
+    }
+
+    @Test
+    public void givenEight_return9699690() {
+        Primorial primorial = new Primorial();
+        int onePrimorial = primorial.findPrimorial(8);
+        assertEquals(9699690, onePrimorial);
     }
 }
